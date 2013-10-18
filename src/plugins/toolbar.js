585d2de693f30154e6c4a4b7b1aef3b0413b799a
Oliver Joseph Ash <oliverjash@icloud.com>
2013-10-18 12:48:26 +0100

    Create Command abstraction

--- src/plugins/toolbar.js
+++ src/plugins/toolbar.js
@@ -1,14 +1,25 @@
 define(function () {
+  function Command(commandName) {
+    this.commandName = commandName;
+  }
+
+  Command.prototype.execute = function () {
+    document.execCommand(this.commandName, false, null);
+  };
+
+  Command.prototype.queryState = function () {
+    return document.queryCommandState(this.commandName);
+  };
+
   return function (toolbar) {
     return function (editable) {
       var button = toolbar.querySelector('button');
 
+      var command = new Command('bold');
       button.addEventListener('click', function () {
-        var command = 'bold';
-        document.execCommand(command, false, null);
-        var state = document.queryCommandState(command);
+        command.execute();
 
-        if (state) {
+        if (command.queryState()) {
           button.classList.add('active');
         } else {
           button.classList.remove('active');
