6784b4d3e14e116cf4edffd8d79d2919dd6cfde2
Oliver Joseph Ash <oliverjash@icloud.com>
2013-10-18 14:11:17 +0100

    Call plugins last

--- example.js
+++ example.js
@@ -14,6 +14,13 @@ require({
 ) {
   var editable = new Editable(document.querySelector('.editor'));
 
+  function updateHTML() {
+    document.querySelector('.editor-html').textContent = editable.el.innerHTML;
+  }
+
+  editable.el.addEventListener('keyup', updateHTML);
+  editable.el.addEventListener('paste', updateHTML);
+
   editable.use(toolbar(document.querySelector('.toolbar')));
   editable.use(sanitize({
     tags: {
@@ -30,11 +37,4 @@ require({
       h2: []
     }
   }));
-
-  function updateHTML() {
-    document.querySelector('.editor-html').textContent = editable.el.innerHTML;
-  }
-
-  editable.el.addEventListener('keyup', updateHTML);
-  editable.el.addEventListener('paste', updateHTML);
 });
