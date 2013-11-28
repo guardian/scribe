9badc21b859ed7ef96d4b25c58863848190d0344
Oliver Joseph Ash <oliverjash@icloud.com>
2013-11-28 17:22:40 +0000

    Revert "0.0.4"
    This reverts commit d90890229d7b29a872b01765e2d4e81beb1eacd4.

--- src/editor.js
+++ src/editor.js
@@ -46,56 +46,54 @@ define([
       // P mode
       this.addInitializer(rootParagraphElement());
     } else {
-      this.addInitializer(function () {
-        // BR mode
-        /**
-         * Firefox has a `insertBrOnReturn` command, but this is not a part of
-         * any standard. One day we might have an `insertLineBreak` command,
-         * proposed by this spec:
-         * https://dvcs.w3.org/hg/editing/raw-file/tip/editing.html#the-insertlinebreak-command
-         * As per: http://jsbin.com/IQUraXA/1/edit?html,js,output
-         */
-        this.el.addEventListener('keydown', function (event) {
-          if (event.keyCode === 13) { // enter
-            var selection = new Selection();
-            var range = selection.range;
+      // BR mode
+      /**
+       * Firefox has a `insertBrOnReturn` command, but this is not a part of
+       * any standard. One day we might have an `insertLineBreak` command,
+       * proposed by this spec:
+       * https://dvcs.w3.org/hg/editing/raw-file/tip/editing.html#the-insertlinebreak-command
+       * As per: http://jsbin.com/IQUraXA/1/edit?html,js,output
+       */
+      this.el.addEventListener('keydown', function (event) {
+        if (event.keyCode === 13) { // enter
+          var selection = new Selection();
+          var range = selection.range;
 
-            var blockNode = selection.getContaining(function (node) {
-              return node.nodeName === 'LI' || (/^(H[1-6])$/).test(node.nodeName);
-            });
+          var blockNode = selection.getContaining(function (node) {
+            return node.nodeName === 'LI' || (/^(H[1-6])$/).test(node.nodeName);
+          });
 
-            if (! blockNode) {
-              event.preventDefault();
-              // The first <br> is the line break, the second <br> is simply
-              // where the caret will go (and replace).
-              var brNode = document.createElement('br');
-              var caretBrNode = document.createElement('br');
+          if (! blockNode) {
+            event.preventDefault();
+            // The first <br> is the line break, the second <br> is simply
+            // where the caret will go (and replace).
+            var brNode = document.createElement('br');
+            var caretBrNode = document.createElement('br');
 
-              range.insertNode(brNode);
-              // After inserting the BR into the range is no longer collapsed, so
-              // we have to collapse it again.
-              range.collapse();
-              range.insertNode(caretBrNode);
+            range.insertNode(brNode);
+            // After inserting the BR into the range is no longer collapsed, so
+            // we have to collapse it again.
+            range.collapse();
+            range.insertNode(caretBrNode);
 
-              var newRange = new window.Range();
+            var newRange = new window.Range();
 
-              newRange.setStartAfter(caretBrNode, 0);
-              newRange.setEndAfter(caretBrNode, 0);
+            newRange.setStartAfter(caretBrNode, 0);
+            newRange.setEndAfter(caretBrNode, 0);
 
-              selection.selection.removeAllRanges();
-              selection.selection.addRange(newRange);
+            selection.selection.removeAllRanges();
+            selection.selection.addRange(newRange);
 
-              this.pushHistory();
-              this.trigger('content-changed');
-            }
+            this.pushHistory();
+            this.trigger('content-changed');
           }
-        }.bind(this));
-
-        if (this.getHTML() === '') {
-          this.pushHistory();
-          this.trigger('content-changed');
         }
       }.bind(this));
+
+      if (this.getHTML() === '') {
+        this.pushHistory();
+        this.trigger('content-changed');
+      }
     }
 
     this.use(commands.insertList());
