d8668f7407483316589ec9c8d26f3546b2fa78d0
Oliver Joseph Ash <oliverjash@icloud.com>
2013-12-03 15:53:11 +0000

    Add Lodash

--- example.js
+++ example.js
@@ -2,13 +2,19 @@ require({
   baseUrl: './src',
   paths: {
     'event-emitter': '../bower_components/eventEmitter/EventEmitter',
-    'html-janitor': '../bower_components/html-janitor/src/html-janitor'
+    'html-janitor': '../bower_components/html-janitor/src/html-janitor',
   },
   shim: {
     'event-emitter': {
       exports: 'EventEmitter'
     }
-  }
+  },
+  packages: [
+    {
+      name: 'lodash',
+      location: '../bower_components/lodash-amd/modern'
+    }
+  ]
 }, [
   'scribe',
   'api/selection',
