8c5743461bd4e11d044e5ded9fefc110131f6e0c
Oliver Joseph Ash <oliverjash@icloud.com>
2013-12-06 11:14:52 +0000

    Update link prompt command to prepend href protocol if missing

--- src/plugins/link-prompt-command.js
+++ src/plugins/link-prompt-command.js
@@ -23,9 +23,33 @@ define([
         var anchorNode = selection.getContaining(function (node) {
           return node.nodeName === nodeName;
         });
-        var initialUrl = anchorNode ? anchorNode.href : 'http://';
-        var url = window.prompt('Enter a URL.', initialUrl);
-        SimpleCommand.prototype.execute.call(this, url);
+        var initialLink = anchorNode ? anchorNode.href : 'http://';
+        var link = window.prompt('Enter a link.', initialLink);
+
+        // FIXME: I don't like how plugins like this do so much. Is there a way
+        // to compose?
+
+        // Prepend href protocol if missing
+        // For emails we just look for a `@` symbol as it is easier.
+        if (! /^mailto\:/.test(link) && /@/.test(link)) {
+          var shouldPrefixEmail = window.confirm(
+            'The URL you entered appears to be an email address. ' +
+            'Do you want to add the required “mailto:” prefix?'
+          );
+          if (shouldPrefixEmail) {
+            link = 'mailto:' + link;
+          }
+        } else if (! /^https?\:\/\//.test(link)) {
+          var shouldPrefixLink = window.confirm(
+            'The URL you entered appears to be a link. ' +
+            'Do you want to add the required “http://” prefix?'
+          );
+          if (shouldPrefixLink) {
+            link = 'http://' + link;
+          }
+        }
+
+        SimpleCommand.prototype.execute.call(this, link);
       };
 
       scribe.commands.linkPrompt = linkPromptCommand;
