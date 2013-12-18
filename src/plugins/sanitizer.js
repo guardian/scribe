45da35630f151ccae5c804ddf3a8405e80ede581
Oliver Joseph Ash <oliverjash@icloud.com>
2013-12-18 12:42:40 +0000

    Tidy up

--- src/plugins/sanitizer.js
+++ src/plugins/sanitizer.js
@@ -13,21 +13,10 @@ define([
 
   return function (config) {
     return function (scribe) {
-      return sanitizer(scribe, config);
+      var janitor = new HTMLJanitor(config);
+
+      scribe.formatter.formatters.push(janitor.clean.bind(janitor));
     };
   };
-
-  /**
-   * Initializes sanitizer plugin on Scribe instance.
-   *
-   * @param {Scribe} scribe
-   * @param {Object} config For configuring the janitor
-   * @return {self}
-   */
-  function sanitizer(scribe, config) {
-    var janitor = new HTMLJanitor(config);
-
-    scribe.formatter.formatters.push(janitor.clean.bind(janitor));
-  }
 
 });
