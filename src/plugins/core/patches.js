43ae6ea1b25977cc6eddfef52c3cb9c0995d04d3
Oliver Joseph Ash <oliverjash@icloud.com>
2013-12-19 11:07:34 +0000

    Document commands

--- src/plugins/core/patches.js
+++ src/plugins/core/patches.js
@@ -12,6 +12,12 @@ define([
   emptyWhenDeleting
 ) {
 
+  /**
+   * Command patches browser inconsistencies. They do not perform core features
+   * of the editor, such as ensuring P elements are created when
+   * applying/unapplying commands — that is the job of the core commands.
+   */
+
   'use strict';
 
   return {
