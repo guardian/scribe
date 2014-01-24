936bc31fca7a6bd7ba3007b14895ce60f6906d8c
Oliver Joseph Ash <oliverjash@icloud.com>
2014-01-24 12:41:08 +0000

    Add max stack size for the undo manager

--- src/undo-manager.js
+++ src/undo-manager.js
@@ -8,10 +8,17 @@ define(function () {
     this.stack = [];
   }
 
+  UndoManager.prototype.maxStackSize = 100;
+
   UndoManager.prototype.push = function (item) {
     console.log('UndoManager.push: %s', item);
     this.stack.length = ++this.position;
     this.stack.push(item);
+
+    if (this.stack.length === this.maxStackSize + 1) {
+      this.stack.shift();
+      --this.position;
+    }
   };
 
   UndoManager.prototype.undo = function () {
