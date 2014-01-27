24cc4b06dbbcf4f9818c4490bd48e654a6e31509
Oliver Joseph Ash <oliverjash@icloud.com>
2014-01-27 16:13:58 +0000

    Only convert new lines to HTML for plain text

--- src/plugins/convert-new-lines-to-html.js
+++ src/plugins/convert-new-lines-to-html.js
@@ -5,7 +5,10 @@ define(function () {
   return function () {
     return function (scribe) {
       scribe.formatter.formatters.push(function (html) {
-        return html.replace(/\n([ \t]*\n)+/g, '</p><p>').replace(/\n/g, '<br>');
+        // Simple heuristic. Might need improving?
+        var isHtml = html.match('<p>');
+        var isPlainText = ! isHtml;
+        return isPlainText && html.replace(/\n([ \t]*\n)+/g, '</p><p>').replace(/\n/g, '<br>') || html;
       });
     };
   };
