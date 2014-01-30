668ffae8c79079482d43e357631ce2960d2a2d24
Oliver Joseph Ash <oliverjash@icloud.com>
2014-01-30 14:41:47 +0000

    Move event patches out into separate file

--- src/plugins/core/events.js
+++ src/plugins/core/events.js
@@ -90,65 +90,6 @@ define(function () {
         });
       }
 
-      /**
-       * Chrome: Altering the paragraph structure by pressing
-       * <backspace> or <delete> (merging/deleting paragraphs) sometimes
-       * results in the application of a line-height attribute to the
-       * contents of the paragraph, either onto existing elements or
-       * by wrapping text in a span.
-       * As per: http://jsbin.com/isIdoKA/4/edit?html,css,js,output
-       */
-      // TODO: do we need to run this on every key press, or could we
-      //       detect when the issue may have occurred?
-      // TODO: run in a transaction so as to record the change? how do
-      //       we know in advance whether there will be a change though?
-      if (scribe.allowsBlockElements()) {
-        scribe.el.addEventListener('keyup', function (event) {
-          if (event.keyCode === 8 || event.keyCode === 46) { // backspace or delete
-
-            var selection = new scribe.api.Selection();
-            var range = selection.range;
-
-            if (range.collapsed) {
-              var containerPElement = selection.getContaining(function (node) {
-                return node.nodeName === 'P';
-              });
-              if (containerPElement) {
-                // Store the caret position
-                selection.placeMarkers();
-
-                // We clone the childNodes into an Array so that it's
-                // not affected by any manipulation below when we
-                // iterate over it
-                var pElementChildNodes = Array.prototype.slice.call(containerPElement.childNodes);
-                pElementChildNodes.forEach(function(pElementChildNode) {
-                  if (pElementChildNode.nodeName === 'SPAN') {
-                    // Unwrap any SPAN that has been inserted
-                    var spanElement = new scribe.api.Element(pElementChildNode);
-                    spanElement.unwrap();
-                  } else if (pElementChildNode.nodeType === Node.ELEMENT_NODE) {
-                    /**
-                     * If the paragraph contains inline elements such as
-                     * A, B, or I, Chrome will also append an inline style for
-                     * `line-height` on those elements, so we remove it here.
-                     */
-                    pElementChildNode.style.lineHeight = null;
-
-                    // There probably wasn’t a `style` attribute before, so
-                    // remove it if it is now empty.
-                    if (pElementChildNode.getAttribute('style') === '') {
-                      pElementChildNode.removeAttribute('style');
-                    }
-                  }
-                });
-
-                selection.selectMarkers();
-              }
-            }
-          }
-        });
-      }
-
 
       /**
        * Run formatters on paste
