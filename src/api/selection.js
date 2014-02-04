6e0074917cb0907b6ea3714e86a2fb413b36890b
Oliver Joseph Ash <oliverjash@icloud.com>
2014-02-04 14:36:50 +0000

    Tidy up

--- src/api/selection.js
+++ src/api/selection.js
@@ -13,7 +13,7 @@ define(function () {
 
     Selection.prototype.getContaining = function (nodeFilter) {
       var node = new scribe.api.Node(this.range.commonAncestorContainer);
-      return nodeFilter(node.node) && node.node || node.getAncestor(nodeFilter);
+      return nodeFilter(node.node) ? node.node : node.getAncestor(nodeFilter);
     };
 
     Selection.prototype.placeMarkers = function () {
