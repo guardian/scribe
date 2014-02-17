1c55ef7fb2da88252d72c1fa075aa2eb6f63d7b9
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-02-17 16:07:38 +0000

    Also use transaction for smart lists

--- src/plugins/smart-lists.js
+++ src/plugins/smart-lists.js
@@ -82,13 +82,12 @@ define(function () {
           // Ignore the typed character
           event.preventDefault();
 
-          scribe.getCommand(listCommand).execute();
+          scribe.transactionManager.run(function() {
+            scribe.getCommand(listCommand).execute();
 
-          // Clear "* "/etc from the list item
-          removeSelectedTextNode();
-
-          scribe.pushHistory();
-          scribe.trigger('content-changed');
+            // Clear "* "/etc from the list item
+            removeSelectedTextNode();
+          });
         }
       }
 
