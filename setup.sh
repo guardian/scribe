d0c6a985349a5095918af0e7de6112c3bd841e16
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-04-09 17:54:11 +0100

    More explicit versioning

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ bower install
 
 # Download the selenium JAR
 SELENIUM_VERSION=2.41.0
+SELENIUM_MINOR_VERSION=2.41
 mkdir -p vendor
 wget -O vendor/selenium-server-standalone-$SELENIUM_VERSION.jar \
-  https://selenium-release.storage.googleapis.com/2.41/selenium-server-standalone-$SELENIUM_VERSION.jar
+  https://selenium-release.storage.googleapis.com/$SELENIUM_MINOR_VERSION/selenium-server-standalone-$SELENIUM_VERSION.jar
