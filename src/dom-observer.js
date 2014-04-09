068ff586e13d6ea31c9b33e9b671c0a30e1d7c71
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-04-09 17:46:52 +0100

    Add missing files

---
+++ src/dom-observer.js
@@ -0,0 +1,57 @@
+define([
+  'lodash-modern/arrays/flatten'
+], function (
+  flatten
+) {
+
+  function observeDomChanges(el, callback) {
+    function notEmptyTextNode(node) {
+      return ! (node.nodeType === Node.TEXT_NODE && node.textContent === '');
+    }
+
+    function notSelectionMarkerNode(node) {
+      return ! (node.nodeType === Node.ELEMENT_NODE && node.className === 'scribe-marker');
+    }
+
+    function includeRealMutations(mutations) {
+      var allChangedNodes = flatten(mutations.map(function(mutation) {
+        var added = Array.prototype.slice.call(mutation.addedNodes);
+        var removed = Array.prototype.slice.call(mutation.removedNodes);
+        return added.concat(removed);
+      }));
+
+      var realChangedNodes = allChangedNodes.
+            filter(notEmptyTextNode).
+            filter(notSelectionMarkerNode);
+
+      return realChangedNodes.length > 0;
+    }
+
+
+    // Flag to avoid running recursively
+    var runningPostMutation = false;
+    var observer = new MutationObserver(function(mutations) {
+      if (! runningPostMutation && includeRealMutations(mutations)) {
+        runningPostMutation = true;
+
+        callback();
+
+        // We must yield to let any mutation we caused be triggered
+        // in the next cycle
+        setTimeout(function() {
+          runningPostMutation = false;
+        }, 0);
+      }
+    });
+
+    observer.observe(el, {
+      attributes: true,
+      childList: true,
+      subtree: true
+    });
+
+    return observer;
+  }
+
+  return observeDomChanges;
+});
