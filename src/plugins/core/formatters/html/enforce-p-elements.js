67516262e9c0bf534f89e8b461fa27a630e2aa10
Oliver Joseph Ash <oliverjash@icloud.com>
2014-04-30 15:12:46 +0100

    Add all headings as block elements

--- src/plugins/core/formatters/html/enforce-p-elements.js
+++ src/plugins/core/formatters/html/enforce-p-elements.js
@@ -25,7 +25,8 @@ define([
 
 
   // TODO: not exhaustive?
-  var blockElementNames = ['P', 'LI', 'DIV', 'BLOCKQUOTE', 'UL', 'OL', 'H2', 'H1'];
+  var blockElementNames = ['P', 'LI', 'DIV', 'BLOCKQUOTE', 'UL', 'OL', 'H1',
+                           'H2', 'H3', 'H4', 'H5', 'H6'];
   function isBlockElement(node) {
     return contains(blockElementNames, node.nodeName);
   }
