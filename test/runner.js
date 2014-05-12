a847275363f3468c5b0df41716d5552c4b438a8e
Oliver Joseph Ash <oliverjash@icloud.com>
2014-05-12 10:54:07 +0100

    Correct issue ref
    [ci skip]

--- test/runner.js
+++ test/runner.js
@@ -13,7 +13,7 @@ var counts = {
  * FIXME: We have to set a ridiculous timeout (20 minutes) because Travis’
  * concurrent builds will sometimes exceed Sauce Labs’ concurrency. We should
  * track the following issue to add an option to Travis for limiting
- * concurrency: https://github.com/travis-ci/travis-ci/issues/1963
+ * concurrency: https://github.com/travis-ci/travis-ci/issues/1366
  */
 mocha.timeout(1200000);
 mocha.reporter('spec');
