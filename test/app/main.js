14c1e8c475971be8f51c8b9ecba27b2a7317be50
Oliver Joseph Ash <oliverjash@icloud.com>
2014-05-13 16:35:13 +0100

    Move config
    [ci skip]

--- test/app/main.js
+++
@@ -1,7 +0,0 @@
-require({
-  paths: {
-    'scribe-common': '../../bower_components/scribe-common/src',
-    'lodash-modern': '../../bower_components/lodash-amd/modern',
-    'html-janitor':  '../../bower_components/html-janitor/html-janitor'
-  }
-});
