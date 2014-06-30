5fdea80eabb1978f576246a0a4bca32b9e3c1ca4
hmgibson23 <hugo.gibson@yahoo.co.uk>
2014-06-30 17:03:06 +0100

    Revert "Do not needlessly run formatters when executing `insertHTML` command"
    This reverts commit cb820e9b6e544ecb1595dd84c5b0ee6a0f61466f.
    
    Conflicts:
    	test/main.spec.js

--- src/scribe.js
+++ src/scribe.js
@@ -204,7 +204,7 @@ define([
   Scribe.prototype.insertHTML = function (html) {
     // TODO: error if the selection is not within the Scribe instance? Or
     // focus the Scribe instance if it is not already focused?
-    this.getCommand('insertHTML').execute(html);
+    this.getCommand('insertHTML').execute(this.htmlFormatter.format(html));
   };
 
   Scribe.prototype.isDebugModeEnabled = function () {
