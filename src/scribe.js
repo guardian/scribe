9f6e3b3b0b935c763285304833460fe6413c28e9
Daniel Nelson <daniel@populr.me>
2014-07-03 10:30:25 -0500

    Auto-select target window with optional override

--- src/scribe.js
+++ src/scribe.js
@@ -44,7 +44,7 @@ define([
       debug: false
     });
     this.commandPatches = {};
-    this.targetWindow = options.targetWindow || window;
+    this.targetWindow = options.targetWindow || el.ownerDocument.defaultView;
     this._plainTextFormatterFactory = new FormatterFactory();
     this._htmlFormatterFactory = new HTMLFormatterFactory();
 
