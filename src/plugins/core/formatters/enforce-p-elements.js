ed7de01414a545dfae096838d14a6ed69d0ac160
Samuel Vasko <samvasko@gmail.com>
2014-07-03 13:28:03 +0200

    Linted and added comment

--- src/plugins/core/formatters/enforce-p-elements.js
+++ src/plugins/core/formatters/enforce-p-elements.js
@@ -5,7 +5,24 @@ define([
   MutationSummary,
   element
 ) {
+
+  /**
+   * Chrome and Firefox: Upon pressing backspace inside of a P, the
+   * browser deletes the paragraph element, leaving the caret (and any
+   * content) outside of any P.
+   *
+   * Firefox: Erasing across multiple paragraphs, or outside of a
+   * whole paragraph (e.g. by ‘Select All’) will leave content outside
+   * of any P.
+   *
+   * Entering a new line in a pristine state state will insert
+   * `<div>`s (in Chrome) or `<br>`s (in Firefox) where previously we
+   * had `<p>`'s. This patches the behaviour of delete/backspace so
+   * that we do not end up in a pristine state.
+   */
+
   'use strict';
+
   return function () {
 
     return function (scribe) {
@@ -15,13 +32,13 @@ define([
       function onPElementMutation (mutations) {
         mutations.forEach(function (summary) {
           // We want only "remove" mutations
-          summary.removed.forEach(function (el) {
+          summary.removed.forEach(function () {
 
             // If removed element was the last one we have to recreate it again
             if (wasLastParagraph()) {
               removeMozBrElements();
               recreateEmptyPElement();
-            };
+            }
 
             // Wrap non-empty text nodes in P
             Array.prototype.forEach.call(scribe.el.childNodes, function (node) {
@@ -29,13 +46,13 @@ define([
               if (node.nodeType === Node.TEXT_NODE && node.data.trim().length) {
                 wrapTextNodeInParagraph(node);
               // Remove parentless BRs
-              } else if (node.nodeName == 'BR') {
+              } else if (node.nodeName === 'BR') {
                 node.remove();
-              };
+              }
             });
           });
         });
-      };
+      }
 
       function recreateEmptyPElement () {
         var pElement = element.createEmptyPElement();
@@ -45,25 +62,25 @@ define([
         newRange.setStartBefore(pElement.firstChild);
         selection.selection.removeAllRanges();
         selection.selection.addRange(newRange);
-      };
+      }
 
       function removeMozBrElements () {
         var brElements = scribe.el.querySelectorAll('br[type="_moz"]');
         Array.prototype.forEach.call(brElements, function (br) {
           br.remove();
         });
-      };
+      }
 
       function wrapTextNodeInParagraph(node) {
         var pElement = element.createEmptyPElement();
         pElement.textContent = node.data;
         scribe.el.replaceChild(pElement, node);
 
-      };
+      }
 
       function wasLastParagraph () {
         return !scribe.el.getElementsByTagName('p').length;
-      };
+      }
 
       new MutationSummary({
         callback: onPElementMutation,
