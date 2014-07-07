0bf34680319e5d852a3a189c4280505b76f51390
hmgibson23 <hugo.gibson@yahoo.co.uk>
2014-07-07 12:44:51 +0100

    Added the optional values for the release script

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ BASE_DIR=$(dirname $0)
 
 if [ $# -ne 1 ];
 then
-    echo "Usage: ./release.sh <version-number>"
+    echo "Usage: ./release.sh <version-number | major | minor | patch | build>"
     exit 1
 fi
 
