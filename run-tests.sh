72b10cec9b45dad00cbc51e8ba1df0125f24571f
Oliver Joseph Ash <oliverjash@icloud.com>
2014-07-16 16:05:45 +0100

    Correctly exit test runner

--- run-tests.sh
+++ run-tests.sh
@@ -3,4 +3,12 @@
 ./node_modules/.bin/http-server -p $TEST_SERVER_PORT --silent &
 PID=$!
 node test/runner
+TEST_RUNNER_EXIT=$?
 kill $PID
+
+if [ $TEST_RUNNER_EXIT == "0" ]; then
+    exit 0
+else
+    exit 1
+fi
+
