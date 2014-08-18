4f9c6b19f1baf98f1255aa02fbdd394578f7fd3b
Adam Wentz <awentz@theonion.com>
2014-08-18 12:25:42 -0500

    Added "paste" HTML formatter group

--- src/scribe.js
+++ src/scribe.js
@@ -279,7 +279,8 @@ define([
       sanitize: Immutable.List(),
       // Normalize content to ensure it is ready for interaction
       normalize: Immutable.List(),
-      'export': Immutable.List()
+      paste: [],
+      export: []
     };
   }
 
@@ -294,6 +295,12 @@ define([
     }, html);
 
     return formatted;
+  };
+
+  HTMLFormatterFactory.prototype.formatPaste = function (html) {
+    return this.formatters.paste.reduce(function (formattedData, formatter) {
+      return formatter(formattedData);
+    }, html);
   };
 
   HTMLFormatterFactory.prototype.formatForExport = function (html) {
