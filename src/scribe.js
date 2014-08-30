3d86d4e63a0844aae27bc05f6d647ff2bb39453d
Chris Sinchok <csinchok@theonion.com>
2014-08-30 14:14:39 -0500

    Missed a line

--- src/scribe.js
+++ src/scribe.js
@@ -91,7 +91,6 @@ define([
 
 
     // Patches
-    this.use(patches.commands.bold());
     this.use(patches.commands.indent());
     this.use(patches.commands.insertHTML());
     this.use(patches.commands.insertList());
