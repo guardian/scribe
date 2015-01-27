afba02b8b6a5a07a2bbc6d6957eee76d36e47fb4
Jon Parsons <jon.parsons@guardian.co.uk>
2015-01-27 10:27:38 +0000

    bump firefox versions

--- circle-ci-parallel.sh
+++ circle-ci-parallel.sh
@@ -1,7 +1,7 @@
 case $CIRCLE_NODE_INDEX in
-  0) BROWSER_NAME='firefox' BROWSER_VERSION='31' npm test ;;
-  1) BROWSER_NAME='firefox' BROWSER_VERSION='32' npm test ;;
-  2) BROWSER_NAME='firefox' BROWSER_VERSION='33' npm test ;;
+  0) BROWSER_NAME='firefox' BROWSER_VERSION='33' npm test ;;
+  1) BROWSER_NAME='firefox' BROWSER_VERSION='34' npm test ;;
+  2) BROWSER_NAME='firefox' BROWSER_VERSION='35' npm test ;;
   3) BROWSER_NAME='chrome'  BROWSER_VERSION='35' npm test ;;
   4) BROWSER_NAME='chrome'  BROWSER_VERSION='36' npm test ;;
   5) BROWSER_NAME='chrome'  BROWSER_VERSION='37' npm test ;;
