1cdf105732bdd5d741a5d472f595befbec1800d8
Robert Rees <robert.rees@guardian.co.uk>
2015-01-29 17:24:55 +0000

    v1.2.3

--- bower_components/scribe/scribe.js
+++ bower_components/scribe/scribe.js
@@ -3808,7 +3808,7 @@ function (elementHelper) {
         this.range = this.selection.getRangeAt(0);
       }
     }
-    
+
     /**
      * @returns Closest ancestor Node satisfying nodeFilter. Undefined if none exist before reaching Scribe container.
      */
@@ -3825,93 +3825,36 @@ function (elementHelper) {
 
     Selection.prototype.placeMarkers = function () {
       var range = this.range;
-      if(!range) { return; }
-
-      var startMarker = document.createElement('em');
-      startMarker.classList.add('scribe-marker');
-      var endMarker = document.createElement('em');
-      endMarker.classList.add('scribe-marker');
-
-      // End marker
-      var rangeEnd = this.range.cloneRange();
-      rangeEnd.collapse(false);
-      rangeEnd.insertNode(endMarker);
-
-      /**
-       * Chrome and Firefox: `Range.insertNode` inserts a bogus text node after
-       * the inserted element. We just remove it. This in turn creates several
-       * bugs when perfoming commands on selections that contain an empty text
-       * node (`removeFormat`, `unlink`).
-       * As per: http://jsbin.com/hajim/5/edit?js,console,output
-       */
-      // TODO: abstract into polyfill for `Range.insertNode`
-      if (endMarker.nextSibling &&
-          endMarker.nextSibling.nodeType === Node.TEXT_NODE
-          && endMarker.nextSibling.data === '') {
-        endMarker.parentNode.removeChild(endMarker.nextSibling);
+      if (!range) {
+        return;
       }
 
-
-
-      /**
-       * Chrome and Firefox: `Range.insertNode` inserts a bogus text node before
-       * the inserted element when the child element is at the start of a block
-       * element. We just remove it.
-       * FIXME: Document why we need to remove this
-       * As per: http://jsbin.com/sifez/1/edit?js,console,output
-       */
-      if (endMarker.previousSibling &&
-          endMarker.previousSibling.nodeType === Node.TEXT_NODE
-          && endMarker.previousSibling.data === '') {
-        endMarker.parentNode.removeChild(endMarker.previousSibling);
+      //we need to ensure that the scribe's element lives within the current document to avoid errors with the range comparison (see below)
+      //one way to do this is to check if it's visible (is this the best way?).
+      if (!scribe.el.offsetParent) {
+        return;
       }
 
+      //we want to ensure that the current selection is within the current scribe node
+      //if this isn't true scribe will place markers within the selections parent
+      //we want to ensure that scribe ONLY places markers within it's own element
+      var scribeNodeRange = document.createRange();
+      scribeNodeRange.selectNodeContents(scribe.el);
 
-      /**
-       * This is meant to test Chrome inserting erroneous text blocks into
-       * the scribe el when focus switches from a scribe.el to a button to
-       * the scribe.el. However, this is impossible to simlulate correctly
-       * in a test.
-       *
-       * This behaviour does not happen in Firefox.
-       *
-       * See http://jsbin.com/quhin/2/edit?js,output,console
-       *
-       * To reproduce the bug, follow the following steps:
-       *    1. Select text and create H2
-       *    2. Move cursor to front of text.
-       *    3. Remove the H2 by clicking the button
-       *    4. Observe that you are left with an empty H2
-       *        after the element.
-       *
-       * The problem is caused by the Range being different, depending on
-       * the position of the marker.
-       *
-       * Consider the following two scenarios.
-       *
-       * A)
-       *   1. scribe.el contains: ["1", <em>scribe-marker</em>]
-       *   2. Click button and click the right of to scribe.el
-       *   3. scribe.el contains: ["1", <em>scribe-marker</em>. #text]
-       *
-       *   This is wrong but does not cause the problem.
-       *
-       * B)
-       *   1. scribe.el contains: ["1", <em>scribe-marker</em>]
-       *   2. Click button and click to left of scribe.el
-       *   3. scribe.el contains: [#text, <em>scribe-marker</em>, "1"]
-       *
-       * The second example sets the range in the wrong place, meaning
-       * that in the second case the formatBlock is executed on the wrong
-       * element [the text node] leaving the empty H2 behind.
-       **/
+      var selectionStartWithinScribeElementStart = this.range.compareBoundaryPoints(Range.START_TO_START, scribeNodeRange) >= 0;
+      var selectionEndWithinScribeElementEnd = this.range.compareBoundaryPoints(Range.END_TO_END, scribeNodeRange) <= 0;
 
+      if (selectionStartWithinScribeElementStart && selectionEndWithinScribeElementEnd) {
 
-      if (! this.selection.isCollapsed) {
-        // Start marker
-        var rangeStart = this.range.cloneRange();
-        rangeStart.collapse(true);
-        rangeStart.insertNode(startMarker);
+        var startMarker = document.createElement('em');
+        startMarker.classList.add('scribe-marker');
+        var endMarker = document.createElement('em');
+        endMarker.classList.add('scribe-marker');
+
+        // End marker
+        var rangeEnd = this.range.cloneRange();
+        rangeEnd.collapse(false);
+        rangeEnd.insertNode(endMarker);
 
         /**
          * Chrome and Firefox: `Range.insertNode` inserts a bogus text node after
@@ -3921,29 +3864,106 @@ function (elementHelper) {
          * As per: http://jsbin.com/hajim/5/edit?js,console,output
          */
         // TODO: abstract into polyfill for `Range.insertNode`
-        if (startMarker.nextSibling &&
-            startMarker.nextSibling.nodeType === Node.TEXT_NODE
-            && startMarker.nextSibling.data === '') {
-          startMarker.parentNode.removeChild(startMarker.nextSibling);
+        if (endMarker.nextSibling &&
+            endMarker.nextSibling.nodeType === Node.TEXT_NODE
+            && endMarker.nextSibling.data === '') {
+          endMarker.parentNode.removeChild(endMarker.nextSibling);
         }
 
+
+
         /**
-         * Chrome and Firefox: `Range.insertNode` inserts a bogus text node
-         * before the inserted element when the child element is at the start of
-         * a block element. We just remove it.
+         * Chrome and Firefox: `Range.insertNode` inserts a bogus text node before
+         * the inserted element when the child element is at the start of a block
+         * element. We just remove it.
          * FIXME: Document why we need to remove this
          * As per: http://jsbin.com/sifez/1/edit?js,console,output
          */
-        if (startMarker.previousSibling &&
-            startMarker.previousSibling.nodeType === Node.TEXT_NODE
-            && startMarker.previousSibling.data === '') {
-          startMarker.parentNode.removeChild(startMarker.previousSibling);
+        if (endMarker.previousSibling &&
+            endMarker.previousSibling.nodeType === Node.TEXT_NODE
+            && endMarker.previousSibling.data === '') {
+          endMarker.parentNode.removeChild(endMarker.previousSibling);
         }
+
+
+        /**
+         * This is meant to test Chrome inserting erroneous text blocks into
+         * the scribe el when focus switches from a scribe.el to a button to
+         * the scribe.el. However, this is impossible to simlulate correctly
+         * in a test.
+         *
+         * This behaviour does not happen in Firefox.
+         *
+         * See http://jsbin.com/quhin/2/edit?js,output,console
+         *
+         * To reproduce the bug, follow the following steps:
+         *    1. Select text and create H2
+         *    2. Move cursor to front of text.
+         *    3. Remove the H2 by clicking the button
+         *    4. Observe that you are left with an empty H2
+         *        after the element.
+         *
+         * The problem is caused by the Range being different, depending on
+         * the position of the marker.
+         *
+         * Consider the following two scenarios.
+         *
+         * A)
+         *   1. scribe.el contains: ["1", <em>scribe-marker</em>]
+         *   2. Click button and click the right of to scribe.el
+         *   3. scribe.el contains: ["1", <em>scribe-marker</em>. #text]
+         *
+         *   This is wrong but does not cause the problem.
+         *
+         * B)
+         *   1. scribe.el contains: ["1", <em>scribe-marker</em>]
+         *   2. Click button and click to left of scribe.el
+         *   3. scribe.el contains: [#text, <em>scribe-marker</em>, "1"]
+         *
+         * The second example sets the range in the wrong place, meaning
+         * that in the second case the formatBlock is executed on the wrong
+         * element [the text node] leaving the empty H2 behind.
+         **/
+
+
+        if (! this.selection.isCollapsed) {
+          // Start marker
+          var rangeStart = this.range.cloneRange();
+          rangeStart.collapse(true);
+          rangeStart.insertNode(startMarker);
+
+          /**
+           * Chrome and Firefox: `Range.insertNode` inserts a bogus text node after
+           * the inserted element. We just remove it. This in turn creates several
+           * bugs when perfoming commands on selections that contain an empty text
+           * node (`removeFormat`, `unlink`).
+           * As per: http://jsbin.com/hajim/5/edit?js,console,output
+           */
+          // TODO: abstract into polyfill for `Range.insertNode`
+          if (startMarker.nextSibling &&
+              startMarker.nextSibling.nodeType === Node.TEXT_NODE
+              && startMarker.nextSibling.data === '') {
+            startMarker.parentNode.removeChild(startMarker.nextSibling);
+          }
+
+          /**
+           * Chrome and Firefox: `Range.insertNode` inserts a bogus text node
+           * before the inserted element when the child element is at the start of
+           * a block element. We just remove it.
+           * FIXME: Document why we need to remove this
+           * As per: http://jsbin.com/sifez/1/edit?js,console,output
+           */
+          if (startMarker.previousSibling &&
+              startMarker.previousSibling.nodeType === Node.TEXT_NODE
+              && startMarker.previousSibling.data === '') {
+            startMarker.parentNode.removeChild(startMarker.previousSibling);
+          }
+        }
+
+
+        this.selection.removeAllRanges();
+        this.selection.addRange(this.range);
       }
-
-
-      this.selection.removeAllRanges();
-      this.selection.addRange(this.range);
     };
 
     Selection.prototype.getMarkers = function () {
@@ -4287,46 +4307,6 @@ define('lodash-amd/modern/arrays/pull',[], function() {
   }
 
   return pull;
-});
-
-define('event-emitter',['lodash-amd/modern/arrays/pull'], function (pull) {
-
-  
-
-  // TODO: once
-  // TODO: unit test
-  // Good example of a complete(?) implementation: https://github.com/Wolfy87/EventEmitter
-  function EventEmitter() {
-    this._listeners = {};
-  }
-
-  EventEmitter.prototype.on = function (eventName, fn) {
-    var listeners = this._listeners[eventName] || [];
-
-    listeners.push(fn);
-
-    this._listeners[eventName] = listeners;
-  };
-
-  EventEmitter.prototype.off = function (eventName, fn) {
-    var listeners = this._listeners[eventName] || [];
-    if (fn) {
-      pull(listeners, fn);
-    } else {
-      delete this._listeners[eventName];
-    }
-  };
-
-  EventEmitter.prototype.trigger = function (eventName, args) {
-    var listeners = this._listeners[eventName] || [];
-
-    listeners.forEach(function (listener) {
-      listener.apply(null, args);
-    });
-  };
-
-  return EventEmitter;
-
 });
 
 /**
@@ -9193,6 +9173,45 @@ define('event-emitter',['lodash-amd/modern/arrays/pull'], function (pull) {
   return Immutable;
 
 }));
+define('event-emitter',['lodash-amd/modern/arrays/pull',
+  'immutable/dist/immutable'], function (pull, Immutable) {
+
+  
+
+  // TODO: once
+  // TODO: unit test
+  // Good example of a complete(?) implementation: https://github.com/Wolfy87/EventEmitter
+  function EventEmitter() {
+    this._listeners = {};
+  }
+
+  EventEmitter.prototype.on = function (eventName, fn) {
+    var listeners = this._listeners[eventName] || Immutable.Set();
+
+    this._listeners[eventName] = listeners.add(fn);
+  };
+
+  EventEmitter.prototype.off = function (eventName, fn) {
+    var listeners = this._listeners[eventName] || Immutable.Set();
+    if (fn) {
+      listeners = listeners.delete(fn);
+    } else {
+      listeners = listeners.clear();
+    }
+  };
+
+  EventEmitter.prototype.trigger = function (eventName, args) {
+    var listeners = this._listeners[eventName] || Immutable.Set();
+
+    listeners.forEach(function (listener) {
+      listener.apply(null, args);
+    });
+  };
+
+  return EventEmitter;
+
+});
+
 define('scribe',[
   'lodash-amd/modern/objects/defaults',
   './plugins/core/commands',
