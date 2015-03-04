b08239155c30540dc715affd2fae0369d9de2076
aaalsaleh <aaalsaleh@gmail.com>
2015-03-04 23:38:25 +0300

    Removing unnecessary checks
    Removing unnecessary checks for the new UndoManager.

--- src/plugins/core/events.js
+++ src/plugins/core/events.js
@@ -92,12 +92,6 @@ define([
           // `scribe.setContent`), we do not want to push to the history. (This
           // happens on the first `focus` event).
           if (isEditorActive) {
-            if (scribe.undoManager) {
-              // Discard the last history item, as we're going to be adding
-              // a new clean history item next.
-              scribe.undoManager.undo();
-            }
-
             // Pass content through formatters, place caret back
             scribe.transactionManager.run(runFormatters);
           } else {
