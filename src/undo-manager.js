f341c9a5d184c5a8738e27d5b271b9cde55c4f31
aaalsaleh <aaalsaleh@gmail.com>
2015-03-05 13:18:59 +0300

    Coding style updates (indentations and brances)

--- src/undo-manager.js
+++ src/undo-manager.js
@@ -9,52 +9,63 @@ define(function () {
 		this.length = 0;
 
 		this.transact = function (transaction, merge) {
-			if (arguments.length < 2)
+			if (arguments.length < 2) {
 				throw new TypeError('Not enough arguments to UndoManager.transact.');
+			}
 
 			transaction.execute();
 
 			stack.splice(0, this.position);
-			if (merge && this.length)
+			if (merge && this.length) {
 				stack[0].push(transaction);
-			else
+			}
+			else {
 				stack.unshift([transaction]);
+			}
 			this.position = 0;
 
-			if (limit && stack.length > limit)
+			if (limit && stack.length > limit) {
 				this.length = stack.length = limit;
-			else
+			}
+			else {
 				this.length = stack.length;
+			}
 
-			if (fireEvent)
+			if (fireEvent) {
 				undoScopeHost.dispatchEvent(new CustomEvent('DOMTransaction', {detail: {transactions: stack[0].slice()}, bubbles: true, cancelable: false}));
+			}
 		};
 
 		this.undo = function () {
 			if (this.position < this.length) {
-				for (var i = stack[this.position].length - 1; i >= 0; i--)
+				for (var i = stack[this.position].length - 1; i >= 0; i--) {
 					stack[this.position][i].undo();
+				}
 				this.position++;
 
-				if (fireEvent)
+				if (fireEvent) {
 					undoScopeHost.dispatchEvent(new CustomEvent('undo', {detail: {transactions: stack[this.position - 1].slice()}, bubbles: true, cancelable: false}));
+				}
 			}
 		};
 
 		this.redo = function () {
 			if (this.position > 0) {
-				for (var i = 0, n = stack[this.position - 1].length; i < n; i++)
+				for (var i = 0, n = stack[this.position - 1].length; i < n; i++) {
 					stack[this.position - 1][i].redo();
+				}
 				this.position--;
 
-				if (fireEvent)
+				if (fireEvent) {
 					undoScopeHost.dispatchEvent(new CustomEvent('redo', {detail: {transactions: stack[this.position].slice()}, bubbles: true, cancelable: false}));
+				}
 			}
 		};
 
 		this.item = function (index) {
-			if (index >= 0 && index < this.length)
+			if (index >= 0 && index < this.length) {
 				return stack[index].slice();
+			}
 			return null;
 		};
 
