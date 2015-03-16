5fc3f858d1b15b03ca4ebd3ffe1d601b6ea37b58
Robert Rees <robert.rees@guardian.co.uk>
2015-03-16 17:38:15 +0000

    Removes the tabs from the spec file

--- test/unit/config.spec.js
+++ test/unit/config.spec.js
@@ -12,21 +12,21 @@ var chai = require('chai');
 var expect = chai.expect;
 
 describe('config', function(){
-	it('should normalise unspecified options', function() {
-		expect(config.checkOptions(undefined)).to.exist;
-	});
+  it('should normalise unspecified options', function() {
+    expect(config.checkOptions(undefined)).to.exist;
+  });
 
-	it('should respect overridden options', function() {
-		var checkedOptions = config.checkOptions({allowBlockElements: false});
+  it('should respect overridden options', function() {
+    var checkedOptions = config.checkOptions({allowBlockElements: false});
 
-		expect(checkedOptions.allowBlockElements).to.be.false;
-	});
+    expect(checkedOptions.allowBlockElements).to.be.false;
+  });
 
-	describe('defaults', function() {
-		it('should apply default values', function() {
-			var checkedOptions = config.checkOptions({});
+  describe('defaults', function() {
+    it('should apply default values', function() {
+      var checkedOptions = config.checkOptions({});
 
-			expect(checkedOptions.allowBlockElements).to.be.true;
-		});
-	});
+      expect(checkedOptions.allowBlockElements).to.be.true;
+    });
+  });
 });
