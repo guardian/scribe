fdba4a20b2207b1cd32eb8cdf1740ea7e302f8b7
Abdulrahman Alsaleh <aaalsaleh@gmail.com>
2015-04-05 21:40:42 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,10 +4,8 @@ npm install
 bower install
 
 # Download the selenium JAR
-SELENIUM_VERSION=2.45.0
-SELENIUM_MINOR_VERSION=2.45
+SELENIUM_VERSION=2.41.0
+SELENIUM_MINOR_VERSION=2.41
 mkdir -p vendor
 wget -O vendor/selenium-server-standalone-$SELENIUM_VERSION.jar \
-  https://selenium-release.storage.googleapis.com/$SELENIUM_MINOR_VERSION/selenium-server-standalone-$SELENIUM_VERSION.jar || \
-  curl -o vendor/selenium-server-standalone-$SELENIUM_VERSION.jar \
   https://selenium-release.storage.googleapis.com/$SELENIUM_MINOR_VERSION/selenium-server-standalone-$SELENIUM_VERSION.jar
