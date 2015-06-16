b6a95df8426715790723c841bff54032237327f2
Regis Kuckaertz <regis.kuckaertz@gmail.com>
2015-06-16 20:53:53 +0200

    Reverting changes on this branch coming from faster-firstdeepestchild

--- src/api/children.js
+++ src/api/children.js
@@ -7,12 +7,19 @@ define(function () {
       return node;
     }
 
-    var child = node.firstChild;
-    if( child.nodeName === 'BR' ) {
-      return node;
+    var treeWalker = document.createTreeWalker(node, NodeFilter.SHOW_ALL, null, false);
+    var previousNode = treeWalker.currentNode;
+    if (treeWalker.firstChild()) {
+      // TODO: build list of non-empty elements (used elsewhere)
+      // Do not include non-empty elements
+      if (treeWalker.currentNode.nodeName === 'BR') {
+        return previousNode;
+      } else {
+        return firstDeepestChild(treeWalker.currentNode);
+      }
+    } else {
+      return treeWalker.currentNode;
     }
-
-    return firstDeepestChild(child);
   }
 
   return {
