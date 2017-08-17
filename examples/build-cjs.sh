84118d88e2c34838ae51b4d10894f87d21136e12
shaundillon <shaun.dillon@guardian.co.uk>
2017-08-17 15:20:10 +0100

    switch to npm only module management

--- examples/build-cjs.sh
+++ examples/build-cjs.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 cd ..
-command -v browserify >/dev/null 2>&1 || { echo >&2 "browserify not installed. run 'npm install -g browserify'. Aborting."; exit 1; }
-bower install
-npm install deamdify
+npm install
 npm install scribe-plugin-toolbar
-browserify -g deamdify \
--r ./bower_components/immutable/dist/immutable.js:immutable \
--r ./bower_components/lodash-amd/modern/object/assign.js:lodash-amd/modern/object/assign \
--r ./bower_components/lodash-amd/modern/object/defaults.js:lodash-amd/modern/object/defaults \
--r ./bower_components/lodash-amd/modern/string/escape.js:lodash-amd/modern/string/escape \
+-r ./node_modules/immutable/dist/immutable.js:immutable \
+-r ./node_modules/lodash-amd/modern/object/assign.js:lodash-amd/modern/object/assign \
+-r ./node_modules/lodash-amd/modern/object/defaults.js:lodash-amd/modern/object/defaults \
+-r ./node_modules/lodash-amd/modern/string/escape.js:lodash-amd/modern/string/escape \
 examples/cjs.js > examples/build.js
